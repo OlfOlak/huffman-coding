2fcf2366e7d21d153fdb2a292e5279a8553d1a76
Olaf Olak <olf.olak@gmail.com>
2022-01-10 19:26:47 +0100

    Initial commit.

---
+++ HuffmanCoding/HuffmanCoding/HuffmanCoding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HuffmanCoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
