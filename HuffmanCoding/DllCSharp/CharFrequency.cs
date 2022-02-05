1ab56d2d232aea452a8c623e7b51cfc0ea2b4dbb
polbrys <polbrys@hyperview.pl>
2022-02-05 13:17:31 +0100

    add necessary files

---
+++ HuffmanCoding/DllCSharp/CharFrequency.cs
@@ -0,0 +1,15 @@
+﻿namespace DllCSharp
+
+{
+    public class CharFrequency
+    {
+        public static int[] CountCharFrequency(int[] file, int[] resultFrequency)
+        {
+            foreach(var bytes in file) {
+                resultFrequency[bytes] += 1;
+            }
+
+            return resultFrequency;
+        }
+    }
+}
